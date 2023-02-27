9c262d96d056b5fee72d5f3a8654ab741afd477d
Jiramet <gozji0801@gmail.com>
2023-02-28 01:01:43 +0700

    Add file Program.cs : Input switch

--- Program.cs
+++ Program.cs
@@ -2,7 +2,27 @@
 namespace Homework1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int password;
+            string agency;
+            Console.Write("Password : ");
+            password = int.Parse(Console.ReadLine());
+            Console.Write("Agency : ");
+            agency = Console.ReadLine();
+
+            switch(agency) {
+                case"CIA":
+                Console.WriteLine("Hello CIA");
+                break;
+                case"FBI":
+                Console.WriteLine("Hello FBI");
+                break;
+                case"NSA":
+                Console.WriteLine("Hello NSA");
+                break;
+                default:
+                Console.WriteLine("Error");
+                break;
+            }
         }
     }
 }
